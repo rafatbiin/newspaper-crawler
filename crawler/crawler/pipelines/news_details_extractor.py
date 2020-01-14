f8f70631c1b4e464c336fc0210af47bc1a5651da
Rafat Bin Islam <rafatbiin@gmail.com>
2020-01-14 19:18:48 +0600

    remove html from item and only add tag for prothomalo.com

--- crawler/crawler/pipelines/news_details_extractor.py
+++ crawler/crawler/pipelines/news_details_extractor.py
@@ -1,3 +1,4 @@
+import base64
 import gzip
 from crawler.utils.url_processor import URLProcessor
 from crawler.utils.newspaper.newspaper import Article
@@ -9,14 +10,14 @@ class NewsDetailsExtractorPipeline(object):
         article = self.get_article(item['url'], item['html'], item['language'])
         item['title'] = article.title
         item['text'] = article.text
-        item['html'] = gzip.compress(item['html'])
         item['videos'] = list(article.movies)
         item['domain'] = URLProcessor.get_domain(item['url'])
         item['top_image'] = article.top_image
         item['images'] = list(article.images)
         item['md5'] = URLProcessor.get_md5(item['url'])
-        item['tags'] = self.get_tags(article, spider)
+        self.get_tags(article, item, spider)
 
+        item.pop('html', None)
         return item
 
     def get_article(self, url, html, language):
@@ -25,9 +26,9 @@ class NewsDetailsExtractorPipeline(object):
         article.parse()
         return article
 
-    def get_tags(self, article, spider):
+    def get_tags(self, article, item, spider):
         if len(article.tags) == 0:
-            return None
+            return
         if spider.name != "prothomalo":
-            return None
-        return list(article.tags)
+            return
+        item['tags'] = list(article.tags)
